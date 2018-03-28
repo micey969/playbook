66a9e94004fdf2e268c76e8ad1f58923fe3dde2d
micey969 <micey_969@hotmail.com>
2018-03-28 09:34:52 -0400

    feat(invoice): extra functions added for review on relevance

--- invoice.sol
+++ invoice.sol
@@ -35,8 +35,16 @@ contract Invoicing {
 
     /**
      * Data Type: Invoices Array
-     */
+     *
+     * Reference each invoice by a number
+     */ 
     mapping (uint256 => Invoice) public invoices;
+    
+     /**
+     * Data Type: invoiceAccts Array
+     * Holds all created invoices
+     */
+    uint256[] internal invoiceAccts;
 
     /**
      * Events
@@ -63,6 +71,7 @@ contract Invoicing {
         invoices[_invoiceHash].total = _total;
         invoices[_invoiceHash].status = false;
         invoices[_invoiceHash].balance = 0;
+        invoiceAccts.push(_invoiceHash) -1;
         return true;
     }
 
@@ -73,6 +82,7 @@ contract Invoicing {
      * @param _invoiceHash {uint256} - Content hash of the off-chain invoice
      * @param _paymentHash {uint256} - Content hash of the off-chain payment
      * @return {bool} - True indicates operation was successful
+     *
      */
     function pay(uint256 _invoiceHash, uint256 _paymentHash) public payable returns (bool) {
         invoices[_invoiceHash].payments[_paymentHash].sender = msg.sender;
@@ -90,6 +100,8 @@ contract Invoicing {
      *
      * @param _invoiceHash {uint256} - Content hash of the off-chain invoice
      * @param _claim {uint256} - Content hash of the off-chain claim
+     *
+     * what exactly is a dispute? How can we programmably solve it??
      */
     function dispute(uint256 _invoiceHash, uint256 _claim) public {
 
@@ -101,8 +113,35 @@ contract Invoicing {
      *
      * @notice Will be an attractive attack vector 
      * @param _invoiceHash {uint256} - Content hash of the off-chain invoice
+     * @param _client {uint256} - Address of
      */
-    function refund(uint256 _invoiceHash) private onlyOwner {
+    function refund(uint256 _invoiceHash, address _client) public {
+        _client.transfer(invoices[_invoiceHash].balance);
+        remove(_invoiceHash);
 
+    }
+    
+    /**
+     * Makes an Invoice  null and void in case of errors 
+     *
+     * @param _invoiceHash {uint256} - Content hash of the off-chain invoice
+     */
+    function remove(uint256 _invoiceHash) public returns (bool){
+        delete(invoices[_invoiceHash]);
+        return true;
+    }
+    
+    /**
+     * Displays a list of created Invoice numbers
+     */
+    function getInvoices() view public returns (uint256[]) {
+        return invoiceAccts;
+    }
+    
+    /**
+     * Gives a count of created Invoice records
+     */
+    function countInvoices() view public returns (uint) {
+        return invoiceAccts.length;
     }
 }
